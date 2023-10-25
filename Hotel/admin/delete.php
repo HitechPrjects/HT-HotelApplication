3357d5fb3bfb577f5946ee24b219b277557cdb29
HitechPrjects <youremail@hitechsolutionsproject@gmail.com>
2023-10-25 16:15:45 +0530

    sush commit

--- Hotel/admin/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("index.html");
-include 'connection.php';
-mysqli_query($con,"DELETE FROM addroom WHERE id='" . $_GET["id"] . "'");
-// header("Location:manage");
-echo '<script type="text/javascript"> alert("Details Deleted Successfuly!") 
-document.location.href="manageroom.php"
-</script>';
-?>
